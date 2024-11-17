e810cf95e67f0dda888ba83d1bab2600bdb62698
Hugo Duncan <hugo@hugoduncan.org>
2024-11-17 13:53:25 -0500

    Add selmer impl of AI prompt component

--- components/ai-prompt/src/dado/ai/prompt/core.clj
+++ components/ai-prompt/src/dado/ai/prompt/core.clj
@@ -1,27 +1,54 @@
 (ns dado.ai.prompt.core
-  "Core implementation of the AI Prompt component.")
+  "Core implementation of the AI Prompt component."
+  (:require
+   [clojure.java.io :as io]
+   [selmer.parser :as selmer]
+   [taoensso.telemere :as t]))
 
-(defn read-template [template-name]
-  ;; TODO: Implement reading template file
-  )
+(def ^:private prompt-dir "dev/ai/prompts/")
 
-(defn parse-template [template-content] 
-  ;; TODO: Implement parsing template content
-  )
+(defn- read-template
+  [template-name]
+  (let [template-path (io/file prompt-dir (str template-name ".md"))]
+    (t/trace!
+      {:id ::read-template :data {:template template-name}}
+      (if (.exists template-path)
+        (slurp template-path)
+        (throw (ex-info "Missing template file" {:type :error/missing-template
+                                                 :template template-name}))))))
 
-(defn compose-templates [parsed-templates]
-  ;; TODO: Implement composing templates in order
-  )
+(defn- parse-template
+  [template-name template-content]
+  (t/trace!
+    {:id ::parse-template :data {:template template-name}}
+    (selmer/parse template-content)))
 
-(defn substitute-data [composed-template data]
-  ;; TODO: Implement substituting data values into template
-  )  
+(defn- compose-templates
+  [parsed-templates]
+  (t/trace!
+    {:id ::compose-templates}
+    (apply str parsed-templates)))
 
-(defn construct-prompt [template-names data]
-  (let [;; TODO: 
-        ;; 1. Read and parse all specified templates
-        ;; 2. Compose templates in specified order  
-        ;; 3. Substitute values from data map
-        ]
-    ;; TODO: Return final constructed prompt string
-    ))
+(defn- substitute-data
+  [composed-template data]
+  (t/trace!
+    {:id ::substitute-data :data {:keys (keys data)}}
+    (selmer/render composed-template data)))
+
+(defn construct-prompt
+  [template-names data]
+  (t/trace!
+    {:id ::construct-prompt :data {:templates template-names :keys (keys data)}}
+    (let [templates (map (fn [template-name]
+                           (-> template-name
+                               read-template
+                               (parse-template template-name)))
+                         template-names)
+          composed (compose-templates templates)
+          substituted (substitute-data composed data)]
+      (if-not (re-find #"\{\{.+?\}\}" substituted)
+        substituted
+        (throw (ex-info "Missing data for prompt substitution"
+                        {:type :error/missing-data
+                         :data data
+                         :unresolved-template substituted}))))))
--- components/ai-prompt/test/dado/ai/prompt/interface_test.clj
+++ components/ai-prompt/test/dado/ai/prompt/interface_test.clj
@@ -1,7 +1,33 @@
 (ns dado.ai.prompt.interface-test
   (:require [clojure.test :refer [deftest testing is]]
-            [dado.ai.prompt.interface :as prompt]))
+            [dado.ai.prompt.interface :as prompt]
+            [babashka.fs :as fs]))
 
-;; TODO: Add tests for construct-prompt function
-;; - Test success cases 
-;; - Test error cases (missing template, malformed template, missing data)
+(deftest construct-prompt-test
+  (fs/with-temp-dir [temp-dir {:prefix "dado-prompt-test-"}]
+    (let [prompt-dir (fs/path temp-dir "ai" "prompts")]
+      (fs/create-dirs prompt-dir)
+      (spit (fs/path prompt-dir "template1.md") "Hello {{name}}")
+      (spit (fs/path prompt-dir "template2.md") "Your request is: {{request}}")
+
+      (binding [dado.ai.prompt.core/prompt-dir (str prompt-dir)]
+        (testing "successful prompt construction"
+          (let [result (prompt/construct-prompt ["template1" "template2"]
+                                                {:name "AI" :request "Tell me a joke"})]
+            (is (= "Hello AI\nYour request is: Tell me a joke" result))))
+
+        (testing "missing template"
+          (is (thrown-with-msg? clojure.lang.ExceptionInfo
+                                #"Missing template file"
+                                (prompt/construct-prompt ["missing-template"] {}))))
+
+        (testing "malformed template"
+          (spit (fs/path prompt-dir "bad-template.md") "{{unclosed")
+          (is (thrown-with-msg? selmer.parser.TemplateParserException
+                                #""
+                                (prompt/construct-prompt ["bad-template"] {}))))
+
+        (testing "missing data"
+          (is (thrown-with-msg? clojure.lang.ExceptionInfo
+                                #"Missing data for prompt substitution"
+                                (prompt/construct-prompt ["template1"] {}))))))))
